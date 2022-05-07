a578c68af80df9a726c7af094134a008b2753c3f
Andrew Freeman <andrew.p.freeman@outlook.com>
2022-05-07 12:19:14 -0400

    Final Push

--- assests/js/script.js
+++ assests/js/script.js
@@ -12,19 +12,23 @@ var specialchString = '!@#$%^&*';
 // End of Character pool ****************************
 
 // Password Generate Code****************************
+
 function generatePassword () {
   var passwordLength = prompt("Choose Password length (8-128 Character");
   var specialChar = confirm("Do you want special characters?");
   var lowercase = confirm("Do you want Lowercase characters?");
   var uppercase = confirm("Do you want Uppercase Characters?");
   var numbers = confirm("Do you want Numbers in your password");
+// ^^^^^^^^^^^ Prompts for User input ^^^^^^^^^^^^^^^^^
 
-
+// Rule for if they do not follow direction**************************
   if (!specialChar && !lowercase && !uppercase && !numbers) {
     alert('YOU HAVE FAILED TRY AGAIN');
     return '';
   }
+// End of rule for failure **************************
 
+// Rules for selection of characaters*****************************
   var validCharacters = "";
   var mustHaves = "";
 
@@ -47,7 +51,9 @@ function generatePassword () {
     validCharacters += numbersString;
     mustHaves += numbersString.charAt(Math.floor(Math.random() * numbersString.length))
   }
+// End of rules for selection of characters***********************************
 
+// Path for password being enter into the field ********************************
   var password = "";
 
   for (let i = 0; i < passwordLength; i++) {
@@ -56,17 +62,17 @@ function generatePassword () {
   }
 
   password = password.split("");
-
+// Password going into an array ^^^^^^^^^^^^^^^^^^^^^^
   for (var i = 0; i < mustHaves.length; i++) {
     password [i] = mustHaves[i];
     
   }
 
   return password.join("");
-
-
-
 }
+
+// End of Password Generate code*****************************
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
